b20def20a4bde3a0093467975b65971cd7579cd5
alexandrbogdanov <al1029ex@gmail.com>
2024-11-12 12:24:39 +0100

    path and query parameters added to post router

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,7 +1,20 @@
+from typing import Optional
+
 from fastapi import APIRouter, status, Response
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/blog", tags=["blog"])
 
-@router.post("/new")
-def create_blog():
-    return {"message": "Blog created"}
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool] = None
+
+
+
+@router.post("/new/{id}", status_code=status.HTTP_201_CREATED)
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {"message": "Blog created",
+            "data": blog.model_dump(),
+            "id": id,
+            "version": version}
