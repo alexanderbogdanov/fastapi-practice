870248c077bd7c8dc4b6701f23deb466bd99d152
alexandrbogdanov <al1029ex@gmail.com>
2024-11-12 10:37:53 +0100

    post router added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, status, Response
 from routers.blog_get import router as blog_get_router
+from routers.blog_post import router as blog_post_router
 
 app = FastAPI()
 app.include_router(blog_get_router)
+app.include_router(blog_post_router)
 
 @app.get("/hello")
 def index():
