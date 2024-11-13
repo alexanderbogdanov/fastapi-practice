cc65fb50d5e33a34028e2a8831f1d2b31d6e1cb2
alexandrbogdanov <al1029ex@gmail.com>
2024-11-13 01:42:52 +0100

    dependencies example added

--- routers/blog_get.py
+++ routers/blog_get.py
@@ -1,6 +1,9 @@
 from enum import Enum
+from typing import Optional
 
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status, Response, Depends
+
+from routers.blog_post import required_functionality
 
 router = APIRouter(prefix="/blog", tags=["blog"])
 
@@ -8,14 +11,17 @@ router = APIRouter(prefix="/blog", tags=["blog"])
          summary='Retrieve all blogs',
          description='Retrieve all blogs from the database',
          response_description='List of blogs')
-def get_all_blogs(page: int = 1, page_size: int = 10):
+def get_all_blogs(page: int = 1, page_size: Optional[int] = 10,
+                  req_parameter: dict = Depends(required_functionality)):
     return {"message": "All blogs",
             "page": page,
-            "page_size": page_size}
+            "page_size": page_size,
+            "req_parameter": req_parameter}
 
 
 @router.get("/{id}/comments/{comment_id}", tags=["comment"])
-def get_comment(id: int, comment_id: int, valid: bool = True, username: str = None):
+def get_comment(id: int, comment_id: int, valid: bool = True, username: str = None,
+                  req_parameter: dict = Depends(required_functionality)):
     """
     Simulates retrieving a comment from a blog
     - **id**: The blog id, mandatory path parameter
@@ -25,7 +31,8 @@ def get_comment(id: int, comment_id: int, valid: bool = True, username: str = No
     """
     return {"message": f"Comment with id {comment_id} from blog with id {id}",
             "valid": valid,
-            "username": username}
+            "username": username,
+            "req_parameter": req_parameter}
 
 
 
--- routers/blog_post.py
+++ routers/blog_post.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, HttpUrl
 
 router = APIRouter(prefix="/blog", tags=["blog"])
 
+
 class Image(BaseModel):
     url: HttpUrl
     alias: str
@@ -31,10 +32,10 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
 def create_comment(blog: BlogModel,
                    id: int,
                    comment_title: str = Query(None,
-                                           title="Title of the comment",
-                                           description="Some interesting description for commment_title",
-                                           alias="commentTitle",
-                                           deprecated=True),
+                                              title="Title of the comment",
+                                              description="Some interesting description for commment_title",
+                                              alias="commentTitle",
+                                              deprecated=True),
                    content: str = Body(..., min_length=10, regex="^[a-z\s]*$"),
                    v: Optional[List[str]] = Query(['1.0', '1.2', '1.2'], alias="version"),
                    comment_id: int = Path(..., title="The ID of the comment", ge=5, le=10)):
@@ -46,3 +47,7 @@ def create_comment(blog: BlogModel,
         "version": v,
         'comment_id': comment_id
     }
+
+
+def required_functionality():
+    return {"message": "This is a required functionality"}
