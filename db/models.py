23aed7885c34aebf21c1df29ba53840c6f901e18
alexandrbogdanov <al1029ex@gmail.com>
2024-11-15 16:57:44 +0100

    added models

---
+++ db/models.py
@@ -0,0 +1,12 @@
+import email
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+
+class DBUser(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
